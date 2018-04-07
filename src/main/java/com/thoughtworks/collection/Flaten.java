dbe09bbb75f1ad51f1db1aa7eb65c2bac2d12a51
ctc <1171332103@qq.com>
2018-04-07 15:05:44 +0800

    提交MyMap类，通过所有测试

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,20 +2,39 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.util.List;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Flaten {
 
     Integer[][] array;
+
     public Flaten(Integer[][] array) {
         this.array = array;
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        Stream.of(array)
+                .forEach(e -> {
+                    result.addAll(Arrays.asList(e));
+                });
+        return result;
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        Stream.of(array)
+                .forEach(e -> {
+                    for (int ele : e) {
+                        if (!result.contains(ele)) {
+                            result.add(ele);
+                        }
+                    }
+                });
+        return result;
     }
 }
