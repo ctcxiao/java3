78c81982286b2430b7f573b473619609344df376
ctc <1171332103@qq.com>
2018-04-07 13:21:10 +0800

    提交Filter类，通过所有测试

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -1,31 +1,41 @@
 package com.thoughtworks.collection;
 
+import com.sun.xml.internal.bind.v2.util.CollisionCheckStack;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Filter {
 
-    List<Integer>  array;
+    List<Integer> array;
 
     public Filter(List<Integer> array) {
-     this.array = array;
+        this.array = array;
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        return array.stream()
+                .filter(e -> e % 2 == 0)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        return array.stream()
+                .filter(e -> e % 3 == 0)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        return firstList.stream()
+                .filter(secondList::contains)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+        return array.stream()
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
